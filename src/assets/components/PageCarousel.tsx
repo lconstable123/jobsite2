1de29527d301c3f8347a6f69686a8bb2c6911321
LUKECONSTABLE <luke@lukeconstable.com>
2025-08-14 14:27:07 +1000

    dd

--- src/assets/components/PageCarousel.tsx
+++ src/assets/components/PageCarousel.tsx
@@ -7,43 +7,49 @@ import Page0 from "./Page0";
 import Page1 from "./Page1";
 import CaroselButton from "@/components/ui/CaroselButton";
 import { usePageContext } from "./lib/hooks";
+import { motion } from "framer-motion";
 
 export default function PageCarousel() {
   const { setApi, canScrollNext, canScrollPrev, scrollNext, scrollPrev } =
     usePageContext();
   return (
-    <Carousel
-      className="w-screen"
-      opts={{ align: "center", loop: true }}
-      setApi={setApi}
-      z-index={200}
-    >
-      <CarouselContent className="flex">
-        <CarouselItem
-          id="item1"
-          className="relative flex items-center justify-center"
-        >
-          <Page0 />
-        </CarouselItem>
-        <CarouselItem id="item2" className=" flex items-center justify-center">
-          <Page1 />
-        </CarouselItem>
-      </CarouselContent>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 100 }}>
+      <Carousel
+        className="w-screen"
+        opts={{ align: "center", loop: true }}
+        setApi={setApi}
+        z-index={200}
+      >
+        <CarouselContent className="flex">
+          <CarouselItem
+            id="item1"
+            className="relative flex items-center justify-center"
+          >
+            <Page0 />
+          </CarouselItem>
+          <CarouselItem
+            id="item2"
+            className=" flex items-center justify-center"
+          >
+            <Page1 />
+          </CarouselItem>
+        </CarouselContent>
 
-      <CaroselButton
-        canScrollNext={canScrollNext}
-        canScrollPrev={canScrollPrev}
-        scrollNext={scrollNext}
-        scrollPrev={scrollPrev}
-        side="left"
-      />
-      <CaroselButton
-        canScrollNext={canScrollNext}
-        canScrollPrev={canScrollPrev}
-        scrollNext={scrollNext}
-        scrollPrev={scrollPrev}
-        side="right"
-      />
-    </Carousel>
+        <CaroselButton
+          canScrollNext={canScrollNext}
+          canScrollPrev={canScrollPrev}
+          scrollNext={scrollNext}
+          scrollPrev={scrollPrev}
+          side="left"
+        />
+        <CaroselButton
+          canScrollNext={canScrollNext}
+          canScrollPrev={canScrollPrev}
+          scrollNext={scrollNext}
+          scrollPrev={scrollPrev}
+          side="right"
+        />
+      </Carousel>
+    </motion.div>
   );
 }
